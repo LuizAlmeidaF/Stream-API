cfae995c5f339cb6891229df6f3c6768d9bfa2ef
luizalmeidaf <luiz.p.fraga10@gmail.com>
2026-06-27 12:47:13 -0300

    Praticando junto com aula de Stream API

---
+++ src/functional_interface/ConsumerExemple.java
@@ -0,0 +1,28 @@
+package functional_interface;
+//import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExemple {
+    static void main() {
+        //Criando a lista de numeros
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6);
+
+        //Usando o consumer com expressao lambda para imprimir numeros pares
+        Consumer<Integer> imprimirNumerosPares = numero -> {
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+            }
+        };
+
+        //Usar o consumer para imprimir os numeros pares no stream
+        numeros.forEach(n -> {
+                if (n % 2 == 0) {
+                    System.out.println(n);
+                }
+            }
+        );
+
+    }
+}
